b77b4ef3feaaf3a561cb996a174ac19eec8eac35
Lê Thành Minh <63393230+letamin@users.noreply.github.com>
2020-05-25 02:14:59 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const dataStructureArray = Array.from(document.querySelectorAll(".data-structure
 const tableContentContainer = document.querySelector(".table-contents-container");
 const illustrationContainer = document.querySelector('.illustration-container');
 const codeContainer = document.querySelector('.code-container');
+const burgerButton = document.querySelector('.burger');
 
 const dataStructureObjec = [
     {
@@ -29,6 +30,11 @@ const dataStructureObjec = [
     },
 ];
 
+initialize();
+
+function initialize() {
+    burgerActive();
+}
 dataStructureArray.forEach((data) => {
     data.addEventListener('click', getName);
 });
@@ -62,6 +68,7 @@ function displayTitleTableContainer(dataObject) {
     })
 }
 
+//Display the operations table and event listener when user click on them
 function displayTableContent(dataObject) {
     var dataOperationArray = [];
     var dataOperationUL = document.createElement('ul');
@@ -71,15 +78,15 @@ function displayTableContent(dataObject) {
         dataOperationUL.classList.add('operation');
         dataOperationUL.appendChild(dataOperationArray[index]);
     })
-
     tableContentContainer.appendChild(dataOperationUL)
 
-    //LOOP ALL THE OPERATIONS AND ADD EVENT LISTENER HERE
+    //Loop all the operations and add event listener
     dataOperationArray.forEach(data => {
         data.addEventListener('click', () => {
+            tableContentContainer.classList.remove('burger-active');
             data.classList.remove('toggle-choose');
             switch (data.innerHTML) {
-                //Linked list insert
+                //Linked List insert
                 case 'Insert':
                     removeChooseToggleOperation(dataOperationArray);
                     data.classList.add('toggle-choose');
@@ -88,7 +95,7 @@ function displayTableContent(dataObject) {
                     var linkedListInsertIllustration = document.createElement('div');
                     linkedListInsertIllustration.innerHTML =
                         `<div class="head-null-insert">
-                        <h3>If head = null (There is nothing on the list yet)</h3>
+                        <h3>If head = null (Nothing on the list yet)</h3>
                         <div class="linklist-illu-intro-container">
                             <div class="cell-container">
                                 <div class="cell">37</div>
@@ -103,10 +110,6 @@ function displayTableContent(dataObject) {
                         <div class="normal-insert">
                             <h3>Else (Normal insertion)</h3>
                             <div class="linklist-illu-intro-container">
-                                <div class="cell-container">
-                                    <div class="cell">12</div>
-                                    <div class="cell"><i class="fas fa-long-arrow-alt-right icon"></i></div>
-                                </div>
                                 <div class="cell-container">
                                     <div class="cell">99</div>
                                     <div class="cell"><i class="fas fa-long-arrow-alt-right icon"></i></div>
@@ -127,10 +130,9 @@ function displayTableContent(dataObject) {
                     linkedListInsertIllustration.classList.add('linkList-insert-illu');
                     illustrationContainer.appendChild(linkedListInsertIllustration);
 
-                    var linkedListInsertCode = document.createElement('div');
-                    linkedListInsertCode.classList.add('code-image-container');
-                    codeContainer.appendChild(linkedListInsertCode);
+                    addCodeImage(codeContainer, data.innerHTML, dataObject.name);
                     break;
+                //Linked List search
                 case 'Search':
                     removeChooseToggleOperation(dataOperationArray);
                     data.classList.add('toggle-choose');
@@ -156,30 +158,121 @@ function displayTableContent(dataObject) {
                             <div class="cell-container">
                                 <div class="cell null"></div>
                             </div>
-                        </div>`
-
+                        </div>
+                        <h3 class="text-result">Return true as 37 is in the Linked List</h3>`
                     linkedListSearchIllustration.classList.add('flex-container');
-
                     illustrationContainer.appendChild(linkedListSearchIllustration);
 
-                    const searchCell = document.querySelectorAll('.search-cell');
+                    const searchCells = Array.from(document.querySelectorAll('.search-cell'));
+                    searchCells.forEach((cell, index) => {
+                        cell.style.animation = `lightUp 2s ease ${index + 1.5}s `
+                    })
 
-
-
-
-
+                    addCodeImage(codeContainer, data.innerHTML, dataObject.name);
                     break;
+                //Linked List Delete
                 case 'Delete':
                     removeChooseToggleOperation(dataOperationArray);
                     data.classList.add('toggle-choose');
+                    clearCode();
+                    clearIllutratrion();
+
+                    var linkedListDeleteIllustration = document.createElement('div');
+                    linkedListDeleteIllustration.innerHTML =
+                        `<h3>Delete 37 from the Linked List</h3>
+                        <div class="linklist-illu-intro-container">
+                            <div class="cell-container">
+                                <div class="cell delete-cell">99</div>
+                                <div class="cell"><i class="fas fa-long-arrow-alt-right icon"></i></div>
+                            </div>
+                            <div class="cell-container tobe-deleted">
+                                <div class="cell delete-cell">37</div>
+                                <div class="cell"><i class="fas fa-long-arrow-alt-right icon"></i></div>
+                            </div>
+                            <div class="cell-container remove-move-left">
+                                <div class="cell">27</div>
+                                <div class="cell"><i class="fas fa-long-arrow-alt-right icon"></i></div>
+                            </div>
+                            <div class="cell-container">
+                                <div class="cell null remove-move-left-2"></div>
+                            </div>
+                        </div>
+                        <h3 class="text-result-2">37 is deleted in the Linked List</h3>`
+                    linkedListDeleteIllustration.classList.add('normal-insert');
+                    illustrationContainer.appendChild(linkedListDeleteIllustration);
+
+                    const deleteCells = Array.from(document.querySelectorAll('.delete-cell'));
+                    deleteCells.forEach((cell, index) => {
+                        cell.style.animation = `lightUp 2s ease ${index + 2.4}s `
+                    })
+
+                    addCodeImage(codeContainer, data.innerHTML, dataObject.name);
                     break;
+                //Linked List Traverse
                 case 'Traverse':
                     removeChooseToggleOperation(dataOperationArray);
                     data.classList.add('toggle-choose');
+                    clearCode();
+                    clearIllutratrion();
+
+                    var linkedListTraverseIllustration = document.createElement('div');
+                    linkedListTraverseIllustration.innerHTML =
+                        `<h3>Traverse through the Linked List</h3>
+                        <div class="linklist-illu-intro-container">
+                            <div class="cell-container">
+                                <div class="cell traverse-cell">12</div>
+                                <div class="cell"><i class="fas fa-long-arrow-alt-right icon"></i></div>
+                            </div>
+                            <div class="cell-container">
+                                <div class="cell traverse-cell">99</div>
+                                <div class="cell"><i class="fas fa-long-arrow-alt-right icon"></i></div>
+                            </div>
+                            <div class="cell-container">
+                                <div class="cell traverse-cell">37</div>
+                                <div class="cell"><i class="fas fa-long-arrow-alt-right icon"></i></div>
+                            </div>
+                            <div class="cell-container">
+                                <div class="cell null"></div>
+                            </div>
+                        </div>`
+                    linkedListTraverseIllustration.classList.add('normal-insert');
+                    illustrationContainer.appendChild(linkedListTraverseIllustration);
+
+                    const traverseCells = Array.from(document.querySelectorAll('.traverse-cell'));
+                    traverseCells.forEach((cell, index) => {
+                        cell.style.animation = `lightUp 2s ease ${index + 1.5}s `
+                    })
+
+                    addCodeImage(codeContainer, data.innerHTML, dataObject.name);
                     break;
+                //Linked List Complexity table
                 case 'Complexity':
                     removeChooseToggleOperation(dataOperationArray);
                     data.classList.add('toggle-choose');
+                    clearCode();
+                    clearIllutratrion();
+
+                    var complexityTable = document.createElement('table');
+                    complexityTable.innerHTML =
+                        `<thead>
+                        <tr>
+                            <th scope="col">Access</th>
+                            <th scope="col">Insert</th>
+                            <th scope="col">Search</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>O(n)</td>
+                                <td>O(1)</td>
+                                <td>O(n)</td>
+                                <td>O(n)</td>
+                            </tr>
+                        </tbody>`
+                    complexityTable.classList.add('linkedListComplexityTable')
+                    complexityTable.style.animation = 'appear 1s ease forwards 0.5s'
+                    illustrationContainer.appendChild(complexityTable);
                     break;
             }
         })
@@ -191,12 +284,22 @@ function displayTableContent(dataObject) {
 
 }
 
+//Add the code images
+function addCodeImage(container, operation, dataStructure) {
+    var codeImageContainer = document.createElement('div');
+    codeImageContainer.classList.add('code-image-container');
+    var urlImageName = dataStructure.replace(' ', '_');
+    codeImageContainer.style.setProperty('--img', `url(/image/${urlImageName}_${operation}.png)`);
+    container.appendChild(codeImageContainer);
+}
+
 function removeChooseToggleOperation(dataOperationArray) {
     dataOperationArray.forEach(data => {
         data.classList.remove('toggle-choose');
     })
 }
 
+//Display the object when the user first choose it
 function displayIntro(dataObject) {
     switch (dataObject.name) {
         //Linked list intro
@@ -229,6 +332,12 @@ function displayIntro(dataObject) {
             break;
     }
 
+}
+
+function burgerActive() {
+    burgerButton.addEventListener('click', () => {
+        tableContentContainer.classList.toggle('burger-active');
+    })
 }
 
 //Clear functions. There are clear all on screen, or clear only one of the containers
